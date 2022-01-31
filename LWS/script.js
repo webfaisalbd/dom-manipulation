f977e9f5dbb0d09495fbf5e4790785f07a26daea
unknown <web.faisal.bd@gmail.com>
2022-01-31 15:17:02 +0600

    some methods, example: getElementById, getElementsByTagName, getElementsByClassName

---
+++ LWS/script.js
@@ -0,0 +1,14 @@
+// finding html Elements
+// some methods, example: 
+// getElementById
+// getElementsByTagName
+// getElementsByClassName
+const p=document.getElementById('demo');
+p.innerHTML='Test 0';
+
+console.log(document.getElementsByTagName('p'));
+
+const classMethod=document.getElementsByClassName('anchor');
+console.log(classMethod);
+
+
